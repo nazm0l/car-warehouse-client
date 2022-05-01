c66d1e1e02d490c2a8406f185823625a60aa8c74
nazm0l <tanjirahmednazmul@gmail.com>
2022-05-01 22:54:15 +0600

    blog section added and design

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>Q&A</h2>
+        <div className='container'>
+            <h2 className='mt-5 text-center'>Blogs</h2>
+            <div className='mt-3'>
+                <div className='border border-success p-3 rounded'>
+                    <h4>Difference between javascript and nodejs?</h4>
+                    <p>Ans: </p>
+                </div>
+                <div className='border border-success p-3 rounded mt-3'>
+                    <h4>When should you use nodejs and when should you use mongodb?</h4>
+                    <p>Ans: </p>
+                </div>
+                <div className='border border-success p-3 rounded mt-3'>
+                    <h4>Differences between sql and nosql databases?</h4>
+                    <p>Ans: </p>
+                </div>
+                <div className='border border-success p-3 rounded mt-3'>
+                    <h4>What is the purpose of jwt and how does it work?</h4>
+                    <p>Ans: </p>
+                </div>
+            </div>
         </div>
     );
 };
